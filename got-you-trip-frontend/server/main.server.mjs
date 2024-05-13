6b5f2be43d38eb66b3f158f2601c0bed3c14b1a6
MrPloman <polplanaayet@gmail.com>
2024-05-13 12:48:29 -0500

    deleted builds and deploys

--- got-you-trip-frontend/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-D3KQDNXV.mjs";import"./chunk-ILTPUHG7.mjs";import"./chunk-5WJ4JFKC.mjs";import"./chunk-TNLAMZVO.mjs";import"./chunk-YG54OGXN.mjs";import"./chunk-EAVKCP3Z.mjs";import"./chunk-JA5WQ3PY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
