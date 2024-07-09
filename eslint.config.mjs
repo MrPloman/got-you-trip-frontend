0a9c1047c3ab93acb31d739d380d6a6455fa89d4
MrPloman <polplanaayet@gmail.com>
2024-07-09 15:25:37 +0200

    format files VSC prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-{  ignores: [".angular"]},
+  { ignores: ['.angular'] },
 
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
