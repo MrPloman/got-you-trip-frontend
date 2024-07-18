392c0fd3174d6784ed8da75db5ddc2b08993cca9
MrPloman <polplanaayet@gmail.com>
2024-07-18 19:41:06 +0200

    slider and scrooldown and up almost finished

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 const routes: Routes = [
   {
@@ -12,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
   exports: [RouterModule],
 })
 export class HomeModule {}
--- src/app/shared/components/image-slider/image-slider.component.ts
+++ src/app/shared/components/image-slider/image-slider.component.ts
@@ -1,41 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-slider',
   templateUrl: './image-slider.component.html',
   styleUrl: './image-slider.component.scss',
 })
-export class ImageSliderComponent {
-  sliderArray = [
+export class ImageSliderComponent implements OnInit {
+  sliderArray: { img: string; alt: string; text: string }[] = [
     {
-      img: 'http://bloquo.cc/img/works/1.jpg',
-      alt: '',
-      text: '365 Days Of weddings a year',
+      img: 'https://www.adpersonam.net/wp-content/uploads/2021/09/ad-personam-itinerari-toscana-crete-senesi.jpg',
+      alt: 'a',
+      text: 'a',
     },
     {
-      img: 'http://bloquo.cc/img/works/2.jpg',
-      alt: '',
-      text: '365 Days Of weddings a year',
+      img: 'https://www.frozentimes.net/wordpress/wp-content/gallery/2012-Spain-Landscapes/2012-Spain_Benalmadena_2012-3186.jpg',
+      alt: 'a',
+      text: 'a',
     },
     {
-      img: 'http://bloquo.cc/img/works/3.jpg',
-      alt: '',
-      text: '365 Days Of weddings a year',
-    },
-    {
-      img: 'http://bloquo.cc/img/works/4.jpg',
-      alt: '',
-      text: '365 Days Of weddings a year',
-    },
-    {
-      img: 'http://bloquo.cc/img/works/5.jpg',
-      alt: '',
-      text: '365 Days Of weddings a year',
+      img: 'https://cdn-v2.theculturetrip.com/1280x713/wp-content/uploads/2017/12/france-1805936_1920.webp',
+      alt: 'a',
+      text: 'a',
     },
   ];
-  transform: number | undefined;
+  transform: number;
   selectedIndex = 0;
+  constructor() {
+    this.selectedIndex = 0;
+    this.transform = 100;
+  }
+
+  ngOnInit() {}
+
   selected(x: number) {
+    this.downSelected(x);
+    this.selectedIndex = x;
+  }
+
+  keySelected(x: number) {
     this.downSelected(x);
     this.selectedIndex = x;
   }
@@ -46,5 +48,9 @@ export class ImageSliderComponent {
     if (this.selectedIndex > 4) {
       this.selectedIndex = 0;
     }
+  }
+  sliderAction($event: unknown) {
+    this.selectedIndex = this.selectedIndex + 1;
+    this.transform = 100 - this.selectedIndex * 50;
   }
 }
